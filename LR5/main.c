42afc5aedd600816c2aaf746132cb20753b2f5bd
naiduik-maxim <maks.naud@gmail.com>
2026-03-30 11:35:33 +0300

    LR6

--- LR5/main.c
+++ LR5/main.c
@@ -16,22 +16,22 @@ struct B
 
 
 int main(){
-    struct A np = {'A', 128};
-    struct B tmp;
+    //struct A np = {'A', 128};
+    //struct B tmp;
 
     //Valgrind
-    //struct A *np = malloc(sizeof(struct A));
-    //struct B *tmp = malloc(sizeof(struct B));
+    struct A *np = malloc(sizeof(struct A));
+    struct B *tmp = malloc(sizeof(struct B));
     
-    //np->a = 'A';
-    //np->b = 128;
+    np->a = 'A';
+    np->b = 128;
 
     printf("Sizr of struct A(non packed): %zu\n", sizeof(struct A));
     printf("Sizr of struct B(packed): %zu\n", sizeof(struct B));
 
     memcpy(&tmp, &np, sizeof(struct A));
 
-    //free(np);
-    //free(tmp);
+    free(np);
+    free(tmp);
     return 0;
 }
